51556ad13fe1aeeb9ddbcdb0058e839b06b89556
David Tomberlin <siyegen@gmail.com>
2014-02-07 15:44:59 -0800

    add loader

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ app.get('/endpoint', function(req, res) {
   res.send("Hey", 200);
 });
 
+app.get('/loaderio-851e087db4027ebd1f790f2a07daa019', function(req, res) {
+  res.send("loaderio-851e087db4027ebd1f790f2a07daa019", 200);
+});
+
 app.listen(PORT);
 console.log('start');
